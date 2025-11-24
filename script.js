98928c3560d1bcaee0564a45786e216f66c42c47
Daniel Martínez <marpe.aldodaniel@gmail.com>
2025-11-24 11:35:24 -0600

    fix: Cambio en el código js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hola api clima");
-
 async function fetchWeatherData( latitude, longitude) {
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
     const response = await fetch(url);
@@ -9,20 +7,23 @@ async function fetchWeatherData( latitude, longitude) {
     console.log(data.elevation);
     console.log(data.current_weather);
     console.log(data.current_weather.temperature);
-    console.log(data.current_weather.windspeed);
     return data.current_weather;
-
 }
-//fetchWeatherData( 25.666815, -100.28233);
+// fetchWeatherData( 25.666815, -100.28233);
 async function handleFetchClick(){
-    console.log ("Botón fetch clickeado");
+    console.log("Boton fetch clickeado");
     const latitude = document.getElementById("latitude-input").value;
     const longitude = document.getElementById("longitude-input").value;
-    const currentTemperature= document.getElementById ("temp-display")
-    const currentWindSpeed = document.getElementById ("wind-display");
 
-    const currentWeather= await fetchWeatherData(latitude, longitude);
+    const windDisplay = document.getElementById("wind-display");
+    const currentTemperature = document.getElementById("temp-display");
+    const weatherBox = document.getElementById("weather-result");
+
+    const currentWeather = await fetchWeatherData(latitude, longitude);
+    
     currentTemperature.textContent = currentWeather.temperature;
-    currentWindSpeed.textContent = currentWindSpeed.windspeed;
+    windDisplay.textContent = currentWeather.windspeed;
+
+    weatherBox.classList.remove("hidden");
 
 }
